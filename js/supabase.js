0785827045d4b4242def0141e81b7277bcf4f6d5
Kamran Qafarsoy <kamranqafarsoy@gmail.com>
2026-05-22 19:13:22 +0400

    fix supabase key

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = "https://qcnkjmnywapgnkbttznv.supabase.co";
-const supabaseKey = "SENIN_ANON_KEY";
+
+const supabaseKey = "BURAYA_TAM_ANON_KEY_YAZ";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
